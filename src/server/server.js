4ab90012d2a753cbeca5bb7f0bb065a63c152048
stallone-dev <stallone.developer@gmail.com>
2023-11-06 22:28:56 -0300

    chore(server): Configure server to return also javascript's modules and TS compile correctly

--- src/server/server.js
+++ src/server/server.js
@@ -10,20 +10,24 @@ const hostname = process.env.HOSTNAME || "localhost";
 const port = process.env.HOSTPORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html",
-    });
 
     console.log(req.url);
 
     const home = path.join(__dirname, "..", "public", "index.html");
 
     if(req.url === "/"){
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
+
         fs.readFile(home, "utf-8", (err, data) =>{
             if(err){throw err;}
             res.end(data);
         });
     } else {
+        if(req.url.includes("scripts")){
+            res.writeHead(200, {"Content-Type": "text/javascript",});
+        }
         fs.readFile(path.join(__dirname, "..", "public", req.url), "utf-8", (err, data) =>{
             if(err){throw err;}
             res.end(data);
